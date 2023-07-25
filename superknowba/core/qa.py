4bb177c59014aab3ded2ccf766e8d5c4cfb29843
Richie Youm <seongmin.youm@mail.mcgill.ca>
2023-07-25 03:44:03 -0400

    fix type

--- superknowba/core/qa.py
+++ superknowba/core/qa.py
@@ -1,12 +1,15 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
+from langchain.chains.conversational_retrieval.base import (
+    BaseConversationalRetrievalChain,
+)
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores.base import VectorStore
 
 
 def get_qa_retrieval_chain(
     llm: ChatOpenAI, vectorstore: VectorStore
-) -> ConversationalRetrievalChain:
+) -> BaseConversationalRetrievalChain:
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm, retriever=vectorstore.as_retriever(), memory=memory
